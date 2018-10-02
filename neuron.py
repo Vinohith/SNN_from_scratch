e4c1c9123572952c291ab7046a1c9947574ecf90
Vinohith <vinohith1998@gmail.com>
2018-10-02 11:19:26 +0530

    Updated neuron.py

--- neuron.py
+++ neuron.py
@@ -1,6 +1,5 @@
 import numpy as np
-import random
-import matplotlib.pyplot as plt 
+from parameters import param as pa 
 
 
 # refractory_time = 5   #neuron refractory time (minimum time between subsequent spikes)
@@ -15,22 +14,22 @@ class neuron:
 	def __init__(self):
 		self.refractory_time = 5
 		self.rest_time = -1
-		self.v = 0
+		self.v = pa.v_rest
 
 	def initial(self, th):
 		self.v_threshold = th
 		self.rest_time = -1
-		self.v = 0
+		self.v = pa.v_rest
 
 	def check(self):
 		if self.v >= self.v_threshold:
-			self.v = 0
+			self.v = pa.v_rest
 			return 1
 		elif self.v < 0:
-			self.v = 0
+			self.v = pa.v_rest
 			return 0
 		else:
 			return 0
 
 	def inhibit(self):
-		self.v = -200
+		self.v = pa.v_min
