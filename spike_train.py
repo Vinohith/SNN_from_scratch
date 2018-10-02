45ad497103d6bff084257a0ea561f6da997ee53a
Vinohith <vinohith1998@gmail.com>
2018-10-02 11:57:02 +0530

    Added file spike_train.py

---
+++ spike_train.py
@@ -0,0 +1,20 @@
+import numpy as np 
+from parameters import param as pa
+import math
+
+def encode(pot):
+
+	train = []
+
+	for l in range(pa.pix):
+		for m in range(pa.pix):
+			temp = np.zeros([(pa.T+1), ])
+			freq = math.ceil(0.102*pot[l][m] + 52.02)
+			freq1 = math.ceil(pa.pix / freq)
+			k = freq1
+			while k < pa.T:
+				temp[k] = 1
+				k = k + freq1
+			train.append(temp)
+
+	return train
