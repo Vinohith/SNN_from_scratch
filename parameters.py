460afeb7df14d71b8c164a9bcb7b1897138f17b8
Vinohith <vinohith1998@gmail.com>
2018-10-02 11:19:01 +0530

    Added file parameters.py

---
+++ parameters.py
@@ -0,0 +1,16 @@
+class param:
+	T = 300
+
+	v_rest = 0
+	v_min = -200
+	
+	pix = 28
+	m = pix * pix
+
+	w_max = 1.5
+	w_min = -1.2
+	A_plus = 0.8
+	A_minus = 0.3
+	tau_plus = 8
+	tau_minus = 5
+	sigma = 0.1
